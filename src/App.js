9dc957c13376d8a8fa5d552755ad346c1952c772
Leonardo Casales <leonardo@smartnet.com.ar>
2020-07-22 21:56:57 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [routes, setRoutes] = useState({})
+  
+  const apiUrl = process.env.REACT_APP_API_URL;
+
+  useEffect(() => {
+    const getRoutes = async () => {
+      try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        setRoutes(data)
+        console.log(data)
+        
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    getRoutes();
+  }, [apiUrl])
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>React Node Redis Auth</p>
+        
       </header>
     </div>
   );
